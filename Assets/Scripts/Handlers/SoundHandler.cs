cedccd6e3a77e24d4412f9b391b8c209ed0228a4
Mattias Stjernstedt <mstjernstedt@gmail.com>
2017-04-19 10:12:39 +0900

    added sound to laser

---
+++ Assets/Scripts/Handlers/SoundHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundHandler : MonoBehaviour
+{
+	public AudioSource laserSound;
+
+	void OnEnable()
+	{
+		laserSound.Play();
+	}
+}
